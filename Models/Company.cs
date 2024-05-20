56429ffdd3c50fe61e04d62ac22fdfdb1140bf5c
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-19 20:08:40 -0500

    Modelo de la base de datos

---
+++ Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace SolidTest.Models;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string Names { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Nit { get; set; }
+    public string Logo { get; set; }
+}
