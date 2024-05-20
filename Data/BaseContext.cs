2e53bdec32a0186045c303360f043913a9b1be8d
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-19 20:07:36 -0500

    Conexion con la base de datos

---
+++ Data/BaseContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SolidTest.Models;
+
+namespace SolidTest.Data;
+
+public class BaseContext : DbContext
+{
+     public BaseContext(DbContextOptions<BaseContext> options): base(options){
+            
+        }
+        public DbSet<Company> Companies { get; set; }
+        
+}
